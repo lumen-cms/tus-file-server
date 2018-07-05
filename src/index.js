e511eb0df069a332c03aded39eb91427bd6686cd
Dominic Garms <djgarms@gmail.com>
2018-07-05 21:06:40 +0800

    add dotenv and try s3 file upload

--- src/index.js
+++ src/index.js
@@ -1,18 +1,28 @@
-const {Server, EVENTS, FileStore} = require('tus-node-server')
+const {Server, EVENTS, FileStore, S3Store} = require('tus-node-server')
+require('dotenv').config()
+const isProduction = process.env.NODE_ENV === 'production'
 
 const server = new Server()
+const port = 8000
+const host = '127.0.0.1'
 
-const fileNameFromUrl = (req) => {
-    console.log(req)
-    return req.url.replace(/\//g, '-')
+if (isProduction) {
+    server.datastore = new S3Store({
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        bucket: process.env.AWS_BUCKET,
+        region: process.env.AWS_REGION,
+        partSize: 8 * 1024 * 1024, // each uploaded part will have ~8MB,
+        tmpDirPrefix: 'tus-s3-store',
+        path: '/files'
+    })
+} else {
+    server.datastore = new FileStore({
+        path: '/files'
+        // namingFunction: fileNameFromUrl
+    })
 }
 
-server.datastore = new FileStore({
-    path: '/files',
-    namingFunction: fileNameFromUrl
-})
-
-// add google or s3 FileStore
 
 server.on(EVENTS.EVENT_UPLOAD_COMPLETE, (event) => {
     console.log(`Upload complete for file `, JSON.stringify(event.file))
@@ -26,8 +36,6 @@ server.on(EVENTS.EVENT_ENDPOINT_CREATED, (event) => {
     console.log(`Upload event endpoint created ${event.url}`)
 })
 
-const host = '127.0.0.1'
-const port = 8000
 server.listen({host, port}, () => {
     console.log(`[${new Date().toLocaleTimeString()}] tus server listening at http://${host}:${port}`)
 })
